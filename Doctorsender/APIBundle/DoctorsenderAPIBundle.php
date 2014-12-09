9bab3b821d19c5f3ad96e75704a3b985c17f6bc3
David Perez <davidpv@gmail.com>
2014-12-09 16:49:18 +0100

    builder

--- Doctorsender/APIBundle/DoctorsenderAPIBundle.php
+++ Doctorsender/APIBundle/DoctorsenderAPIBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctorsenderAPIBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
