785c83a1f51e7da7ccbe7ad7a58213ca0c91666d
David Perez <davidpv@gmail.com>
2014-12-09 16:54:07 +0100

    containerBuilder

--- Doctorsender/APIBundle/DoctorsenderAPIBundle.php
+++ Doctorsender/APIBundle/DoctorsenderAPIBundle.php
@@ -3,6 +3,7 @@
 namespace Doctorsender\APIBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DoctorsenderAPIBundle extends Bundle
 {
